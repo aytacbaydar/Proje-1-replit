5bb2f38d3ae37f6a9af1bc72b804e0d423a30598
Aytaç Baydar <60582171+aytacbaydar@users.noreply.github.com>
2025-05-12 22:03:16 +0300

    12.05.2025-pazartesi

--- kimyaogreniyorum/src/app/components/admin-page/admin-page.module.ts
+++ kimyaogreniyorum/src/app/components/admin-page/admin-page.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AdminIndexPageComponent } from './admin-page/admin-index-page/admin-index-page.component';
+import { AdminIndexPageComponent } from './admin-index-page/admin-index-page.component';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
-
-
 
 @NgModule({
   declarations: [AdminIndexPageComponent],
